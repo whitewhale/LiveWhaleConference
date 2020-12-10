ea75d9a378ca1c08cda53680ffcaf273f0c15cdf
nroyall <naomi@whitewhale.net>
2020-12-10 10:59:25 -0800

    Update social media example

--- social-icons/global.config.php
+++ social-icons/global.config.php
@@ -8,15 +8,15 @@ Lets editors choose social icons from the formats menu in the LiveWhale toolbar
 
 */
 
-$_LW->CONFIG['STYLE_FORMATS']=array(
-  array('title'=>'Link styles','selector'=>'a','classes'=>'mce-format-header', 'block'=>'h1'),
-  array('title'=>'Facebook Link','selector'=>'a','classes'=>'facebook-link'),
-  array('title'=>'Twitter Link','selector'=>'a','classes'=>'twitter-link'),
-  array('title'=>'Instagram Link','selector'=>'a','classes'=>'instagram-link'),
-  array('title'=>'Snapchat Link','selector'=>'a','classes'=>'snapchat-link'),
-  array('title'=>'YouTube Link','selector'=>'a','classes'=>'youtube-link'),
-  array('title'=>'Spotify Link','selector'=>'a','classes'=>'spotify-link'),
-);
+$_LW->CONFIG['STYLE_FORMATS']=[
+	'title'=>['Link styles', 'selector'=>'a', 'classes'=>'mce-format-header', 'block'=>'h1'],
+	'title'=>['Facebook Link', 'selector'=>'a','classes'=>'facebook-link'],
+  'title'=>['Twitter Link','selector'=>'a','classes'=>'twitter-link'],
+  'title'=>['Instagram Link','selector'=>'a','classes'=>'instagram-link'],
+  'title'=>['Snapchat Link','selector'=>'a','classes'=>'snapchat-link'],
+  'title'=>['YouTube Link','selector'=>'a','classes'=>'youtube-link'],
+  'title'=>['Spotify Link','selector'=>'a','classes'=>'spotify-link']
+];
 
 
 ?>
